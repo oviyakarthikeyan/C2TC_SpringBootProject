3ed1eae6abe3896eace6a93d725ba66f5336ea2f
oviyakarthikeyan <oviyakarthikeyan29@gmail.com>
2025-09-04 21:04:31 +0530

    This is my springbootbasic project

---
+++ springbootbasic/src/main/java/com/tnsif/springbootbasic/SpringbootbasicApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.springbootbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicApplication.class, args);
+	}
+
+}
