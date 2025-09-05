a506c537a39c8b97214c152fe961471744d162e1
oviyakarthikeyan <oviyakarthikeyan29@gmail.com>
2025-09-05 10:41:03 +0530

    Updated

--- restfulapi1/src/main/java/com/tnsif/restapi/RestControllerExample.java
+++ restfulapi1/src/main/java/com/tnsif/restapi/RestControllerExample.java
@@ -1,6 +1,9 @@
 package com.tnsif.restapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,14 @@ public class RestControllerExample {
 	public String show() {
 		return "Welcome to the TNSIF Capgemini CSR Spring Boot Session..";
 	}
-
+	
+	@GetMapping("/hello")
+	public String greetings(@RequestParam String name) {
+		return "Welcome "+name;
+	}
+	
+	@GetMapping("/hello/{name}")
+	public String message(@PathVariable String name) {
+		return "Welcome "+name;
+	}
 }
